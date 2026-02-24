f3f04b8e6604651e754d17bf5a0366b3447a337f
LailaSlp1 <alunomlmb.sofia@gmail.com>
2026-02-24 14:12:12 -0300

    24.02

---
+++ phpmysql/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<HTML lang="PT-BR">
+
+
+<head>
+    <title>Exemplo php</title>
+</head>
+
+<body>
+    <p> Hoje é </p>
+</body>
+
+</HTML>
+
+<?php
+
+echo "Olá mundo!<br>
+<h3> hoje etsá chovendo muito<\h3>";
+
+echo "<br>-------------------------------------<br>"
+
+$nome = "Carlos";
+$idade = 17;
+$altura = 1.75;
+$adress = "Rua que sobe e desce, 222";
+
+echo "Nome:$nome, Idade:$idade, Altura: $autura, Endereço: $adress"
+
+$mensagem = "Olá!";
+if (print $mensagem) {
+    echo " - Mensagem exibida com sucesso!";
+}
+
+echo "<br>-------------------------------------<br>"
+
+?>
+
+
