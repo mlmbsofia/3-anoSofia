e114a75b30e142a8197a994507aa95843ab27651
LailaSlp1 <alunomlmb.sofia@gmail.com>
2026-03-09 11:30:12 -0300

    aiai

--- phpmysql/tiposDeDados.php
+++ phpmysql/tiposDeDados.php
@@ -1,28 +1,67 @@
 <?php
-    //INTEIROS
-    $ano = 2020;
-    echo $ano;
-    echo "<br>";
+//    //INTEIROS
+//    $ano = 2020;
+//    echo $ano;
+//    echo "<br>";
+//
+//echo "<br>=======================================================<br>";
+//
+//    // PONTO FLUTUANTE
+//    $PI = 3.14;
+//    echo "Valor de PI : $PI";
+//    echo "<br>";
+//
+//    $nome = "Sol Garcia";
+//    $cpf = '1234567890';
+//    echo 'NOME :'.$nome. '<br>'.'CPF: ' .$cpf;
+//
+//    $idade = 18;
+//    if($idade == true){
+//        echo "<br> Idade: ".$idade;
+//    }
+//
+//echo "<br>=======================================================<br>";
+//
+//    //ARRAYS
+//
+//    $nome = ["Joel","Nicole","Sol","Maria"];
+//    foreach($nome as $nomes){
+//        echo "<br>".$nomes;
+//    }
+//
+//echo "<br>=======================================================<br>";
+//
+//    $turma = [
+//        ["nome" => 'Ana', 'nota' => 8.5],
+//        ["nome" => 'Carla', 'nota' => 9.5],
+//        ["nome" => 'Maria', 'nota' => 10],
+//    ];
+//    echo $turma[0]["nome"]. "<br>";
+//    echo $turma[2]["nota"];
+
+echo "<br>=======================================================<br>";
 
 
-    // PONTO FLUTUANTE
-    $PI = 3.14;
-    echo "Valor de PI : $PI";
-    echo "<br>";
+//Atividade
 
     $nome = "Sol Garcia";
-    $cpf = '1234567890';
-    echo 'NOME :'.$nome. '<br>'.'CPF: ' .$cpf;
+        echo "Nome:".$nome;
 
-    $idade = 18;
-    if($idade == true){
-        echo "<br> Idade: ".$idade;
+    $idade = 17;
+        echo "<br>Idade: ".$idade;
+
+    $altura = 1.70;
+        echo "<br>Altura:".$altura;
+
+    $estudante = "aluno";
+    if($estudante == true){
+        echo "<br> Estudante: ". $estudante;
+}
+    $cor = ['roxo', 'rosa', 'preto'];
+    foreach($cor as $cor){
+        echo "<br> ".$cor;
     }
 
-    //ARRAYS
+echo "<br>=======================================================<br>";
 
-    $nome = ["Joel","Nicole","Sol","Maria"];
-    foreach($nome as $nomes){
-        echo "<br>".$nomes;
-    }
 ?>
