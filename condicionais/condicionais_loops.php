154d040a497be08d39f1f68ff88f591c9189c874
LailaSlp1 <alunomlmb.sofia@gmail.com>
2026-03-16 09:30:05 -0300

    as

--- condicionais/condicionais_loops.php
+++ condicionais/condicionais_loops.php
@@ -1,37 +1,67 @@
 <?php
-    //if , elseif, if
-
-//if
-
-$idade = 18;
-    if($idade >= 18){
-        echo "Você é maior de idade!<br>";
-    }
+//if , elseif, if
+//
+////if
+//
+//$idade = 18;
+//    if($idade >= 18){
+//        echo "Você é maior de idade!<br>";
+//    }
+//
+//echo "<br>=======================================================<br>";
+//
+//$diaSemana = 3;
+//
+//switch($diaSemana){
+//
+//    case 1:
+//        echo "domingo<br>";
+//        break;
+//    case 2:
+//        echo "segunda<br>";
+//        break;
+//    case 3:
+//        echo "terça<br>";
+//        break;
+//    case 4:
+//         echo "quarta<br>";
+//         break;
+//    case 5:
+//        echo "quinta<br>";
+//        break;
+//        echo "dia valido <br>";
+//    default:
+//    echo "dia inválido<br>";
+//}
 
 echo "<br>=======================================================<br>";
 
-$diaSemana = 3;
+//exercícios
 
-switch($diaSemana){
+// - Identificação do cliente
 
-    case 1:
-        echo "domingo<br>";
-        break;
-    case 2:
-        echo "segunda<br>";
-        break;
-    case 3:
-        echo "terça<br>";
-        break;
-    case 4:
-         echo "quarta<br>";
-         break;
-    case 5:
-        echo "quinta<br>";
-        break;
-        echo "dia valido<br>";
-    default:
-    echo "dia inválido<br>";
-}
+$nome = "Carlos";
+$idade = 26;
+$cpf = 1234567890;
 
-echo "<br>=======================================================<br>";
+    echo "<br> Nome do cliente:".$nome "<br> Idade:".$idade "<br> CPF:".$cpf;
+
+    echo "<br>=======================================================<br>";
+
+// - Saldo inicial
+
+
+
+// - Pode fazer depósito e mostrar saldo atualizado
+
+
+// - Pode fazer saque e mostrar saldo atualizado
+
+
+// - Pode fazer fazer empréstimo parcelo com juros simples de 20% ao ano
+
+
+// - Pode ver o saldo
+
+
+// - Tem que sair da conta corrente
